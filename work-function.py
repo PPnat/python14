cdd56711c30d6c4c7847de82c85a1ccaf490a01e
PPnat <47552@cru.ac.th>
2022-07-26 14:48:21 +0700

    Function

---
+++ work-function.py
@@ -0,0 +1,12 @@
+from math import pi
+
+
+
+def square(width, height):
+    return width * height
+def triangle(base,height):
+    return 0.5 * base + height
+def circle(radius):
+    return pi * radius * radius
+
+#นายพีรณัฐ พลศิริ ม.6/14 เลขที่ 17
